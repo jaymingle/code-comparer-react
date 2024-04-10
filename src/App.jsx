24b30cb62b9c8644eeeb498ba9731d426fdb2391
Jay Mingle <jkawudi@gmail.com>
2024-04-10 00:57:02 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,17 @@ function App() {
     const [textAreaA, setTextAreaA] = useState('');
     const [textAreaB, setTextAreaB] = useState('');
 
+    const changeHandler = (e, id) => {
+        console.log('Changed')
+    }
+
     console.log('app')
 
   return (
       <div className="App">
           <div className="formA">
               Text A
-              <textarea name="" id="" cols="60" rows="40"/>
+              <textarea value={textAreaA} onChange={e => changeHandler(e, 'inputA')} name="" id="" cols="60" rows="40"/>
 
           </div>
           <div className="formB">
