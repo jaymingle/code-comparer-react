d46d0b25590b88dfa37e488255fc0416e30bcbb8
Jay Mingle <jkawudi@gmail.com>
2024-04-10 01:18:54 +0000

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
     const [textAreaA, setTextAreaA] = useState('');
     const [textAreaB, setTextAreaB] = useState('');
+    const [diffResults, setDiffResults] = useState('');
 
     const dmp = new DiffMatchPatch();
 
@@ -19,18 +20,33 @@ function App() {
         }
     }
 
-  return (
-      <div className="App">
-          <div className="formA">
-              Text A
-              <textarea value={textAreaA} onChange={e => changeHandler(e, 'inputA')} name="" id="" cols="60" rows="40"/>
-          </div>
-          <div className="formB">
-              Text B
-              <textarea value={textAreaB} onChange={e => changeHandler(e, 'inputB')} name="" id="" cols="60" rows="40"/>
-          </div>
-      </div>
-  )
+    const handleDiffCalculation = () => {
+        const diffs = dmp.diff_main(textAreaA, textAreaB);
+        dmp.diff_cleanupSemantic(diffs);
+        const htmlDiff = dmp.diff_prettyHtml(diffs);
+        setDiffResult(htmlDiff);
+    };
+
+
+    return (
+        <div className="auto-margin">
+            <div className="App">
+                <div className="formA">
+                    Text A
+                    <textarea value={textAreaA} onChange={e => changeHandler(e, 'inputA')} name="" id="" cols="60"
+                              rows="40"/>
+                </div>
+                <div className="formB">
+                    Text B
+                    <textarea value={textAreaB} onChange={e => changeHandler(e, 'inputB')} name="" id="" cols="60"
+                              rows="40"/>
+                </div>
+            </div>
+            <button onClick={handleDiffCalculation}>Compare Texts</button>
+            <div className="diff-result" dangerouslySetInnerHTML={{__html: diffResult}}/>
+        </div>
+
+    )
 }
 
 export default App
